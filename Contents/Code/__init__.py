f24ce9def105b32fbbbfb162815110b8f9e34a6a
Sander <sander@plex.tv>
2017-10-11 21:17:30 +0200

    Code layout

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -14,38 +14,37 @@ RE_CLEAN = Regex('[^a-z0-9\-]')
 
 ####################################################################################################
 def Start():
-    # Setup the default attributes for the ObjectContainer
+
     ObjectContainer.title1 = TITLE
 
     HTTP.CacheTime = CACHE_1HOUR
     HTTP.User_Agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.3.18 (KHTML, like Gecko) Version/8.0.3 Safari/600.3.18'
 
-##########################################################################################
+####################################################################################################
 @handler(PREFIX, TITLE, art=ART, thumb=ICON)
 def MainMenu():
+
     oc = ObjectContainer()
-    
-    title = 'Most Recent'
-    oc.add(
-        DirectoryObject(
-            key = Callback(MostRecent, title=title),
-            title = title
-        )
-    )    
-    
+
+    oc.add(DirectoryObject(
+        key = Callback(MostRecent, title='Most Recent'),
+        title = 'Most Recent'
+    ))
+
     for object in GetEntries().objects:
         oc.add(object)
-    
+
     return oc
-    
-##########################################################################################
+
+####################################################################################################
 @route(PREFIX + '/mostrecent')
 def MostRecent(title):
 
-    oc = ObjectContainer(title2 = title)    
+    oc = ObjectContainer(title2 = title)
     json_data = JSON.ObjectFromURL(FEED_URL % ('', 1, MOST_RECENT_ITEMS))
-    
+
     for entry in json_data['entries']:
+
         if entry['pl1$clipType'] not in FULL_EPISODE_TYPES:
             continue
 
@@ -53,50 +52,48 @@ def MostRecent(title):
 
     return oc
 
-##########################################################################################
+####################################################################################################
 @route(PREFIX + '/getentries', depth = int, return_first_found = bool)
 def GetEntries(title="", depth=1, id=None, return_first_found=False):
-    
+
     oc = ObjectContainer(title2 = title)
-    
+
     data = HTTP.Request(MAIN_URL).content
     data = data[1:-1]
     json_data = JSON.ObjectFromString(data)
-    
+
     for item in json_data['items']:
+
         if not item['fullTitle'].startswith("HGTVNEWVC/"):
             continue
-            
+
         if item['depth'] != depth:
             continue
-        
+
         if id:
             if id != item['parentId']:
                 continue
 
         if item['hasReleases'] and depth > 1:
+
             if return_first_found:
                 return Videos(show=item['title'], id=item['id'].split("/")[-1])
             else:
-                oc.add(
-                    DirectoryObject(
-                        key = Callback(Videos, show=item['title'], id=item['id'].split("/")[-1]),
-                        title = item['title']
-                    )
-                )
+                oc.add(DirectoryObject(
+                    key = Callback(Videos, show=item['title'], id=item['id'].split("/")[-1]),
+                    title = item['title']
+                ))
         else:
             if return_first_found:
                 return GetEntries(title=item['title'], depth=depth+1, id=item['id'])
             else:
-                oc.add(
-                    DirectoryObject(
-                        key = Callback(GetEntries, title=item['title'], depth=depth+1, id=item['id']),
-                        title = item['title']
-                    )
-                )
-    
+                oc.add(DirectoryObject(
+                    key = Callback(GetEntries, title=item['title'], depth=depth+1, id=item['id']),
+                    title = item['title']
+                ))
+
     oc.objects.sort(key = lambda obj: obj.title)
-    
+
     if len(oc) > 1:
         return oc
     elif len(oc) == 1:
@@ -104,20 +101,22 @@ def GetEntries(title="", depth=1, id=None, return_first_found=False):
     else:
         return ObjectContainer(header="Sorry", message="Couldn't find any items")
 
-##########################################################################################
+####################################################################################################
 @route(PREFIX + '/clips')
 def Clips(show, id):
+
     return Videos(show=show, id=id, full_episodes_only=False)
 
-##########################################################################################
+####################################################################################################
 @route(PREFIX + '/videos', full_episodes_only = bool)
 def Videos(show, id, full_episodes_only=True):
 
     oc = ObjectContainer(title2 = show)
-    
-    json_data = JSON.ObjectFromURL(FEED_URL % (id, 1, 1000))    
-    
+
+    json_data = JSON.ObjectFromURL(FEED_URL % (id, 1, 1000))
+
     for entry in json_data['entries']:
+
         if full_episodes_only:
             if entry['pl1$clipType'] not in FULL_EPISODE_TYPES:
                 continue
@@ -129,7 +128,7 @@ def Videos(show, id, full_episodes_only=True):
 
     if full_episodes_only:
         clips_oc = Clips(show, id)
-        
+
         if len(clips_oc) > 0:
             if len(oc) > 0:
                 oc.add(DirectoryObject(key=Callback(Clips, show=show, id=id), title="Clips"))
@@ -141,7 +140,7 @@ def Videos(show, id, full_episodes_only=True):
     else:
         return oc
 
-##########################################################################################
+####################################################################################################
 def CreateVideoObject(entry):
 
     title = entry['title']
@@ -154,11 +153,13 @@ def CreateVideoObject(entry):
 
     summary = entry['description'] if 'description' in entry else None
     thumb = entry['defaultThumbnailUrl'] if 'defaultThumbnailUrl' in entry else None
-    
+
     show = None
     season = None
     index = None
+
     if entry['pl1$clipType'] in FULL_EPISODE_TYPES:
+
         show = entry['pl1$show'] if 'pl1$show' in entry else None
         season = int(entry['pl1$season']) if 'pl1$season' in entry else None
         index = int(entry['pl1$episode']) if 'pl1$episode' in entry else None
@@ -166,11 +167,13 @@ def CreateVideoObject(entry):
     originally_available_at = Datetime.FromTimestamp(entry['pubDate'] / 1000).date() if 'pubDate' in entry else None
 
     duration = None
+
     if 'content' in entry:
         if 'duration' in entry['content'][0]:
-            duration = int(float(entry['content'][0]['duration']) * 1000) 
+            duration = int(float(entry['content'][0]['duration']) * 1000)
 
     if entry['pl1$clipType'] in FULL_EPISODE_TYPES:
+
         return EpisodeObject(
             url = url,
             title = title,
@@ -182,7 +185,9 @@ def CreateVideoObject(entry):
             originally_available_at = originally_available_at,
             duration = duration
         )
+
     else:
+
         return VideoClipObject(
             url = url,
             title = title,
@@ -191,4 +196,3 @@ def CreateVideoObject(entry):
             originally_available_at = originally_available_at,
             duration = duration
         )
-
